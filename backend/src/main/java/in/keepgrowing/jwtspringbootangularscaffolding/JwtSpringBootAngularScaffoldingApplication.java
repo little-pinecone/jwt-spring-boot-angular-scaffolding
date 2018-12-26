e2c19464bd0bd65a86586eb237dfc134f94496ad
little_pinecone <marta.szymek.mail@gmail.com>
2018-12-26 18:25:38 +0100

    Initial commit. Spring Boot module prepared.

---
+++ backend/src/main/java/in/keepgrowing/jwtspringbootangularscaffolding/JwtSpringBootAngularScaffoldingApplication.java
@@ -0,0 +1,14 @@
+package in.keepgrowing.jwtspringbootangularscaffolding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtSpringBootAngularScaffoldingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtSpringBootAngularScaffoldingApplication.class, args);
+	}
+
+}
+
---
+++ backend/src/test/java/in/keepgrowing/jwtspringbootangularscaffolding/JwtSpringBootAngularScaffoldingApplicationTests.java
@@ -0,0 +1,17 @@
+package in.keepgrowing.jwtspringbootangularscaffolding;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JwtSpringBootAngularScaffoldingApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
