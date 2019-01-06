1d6b7c76360d2ad98ec02c1af0c63719dd29c87c
little_pinecone <marta.szymek.mail@gmail.com>
2019-01-06 17:28:50 +0100

    Spring Security dependency added to the project.

--- backend/src/test/java/in/keepgrowing/jwtspringbootangularscaffolding/cookie/CookieControllerTest.java
+++ backend/src/test/java/in/keepgrowing/jwtspringbootangularscaffolding/cookie/CookieControllerTest.java
@@ -5,6 +5,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -21,6 +22,7 @@ public class CookieControllerTest {
     private MockMvc mvc;
 
     @Test
+    @WithMockUser(roles = "USER")
     public void getsCookies() throws Exception {
         mvc.perform(get("/api/cookies")
                 .contentType(MediaType.APPLICATION_JSON))
