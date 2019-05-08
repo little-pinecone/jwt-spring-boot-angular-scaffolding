e26dbc7649a0bb48bbf0f2f66ae32179490d120a
little_pinecone <marta.szymek.mail@gmail.com>
2019-05-08 21:15:47 +0200

    Frontend redirects users to the login page in case of a 401 error from the API.

--- frontend/src/main/angular/src/app/auth/interceptors/jwt.token.interceptor.ts
+++ frontend/src/main/angular/src/app/auth/interceptors/jwt.token.interceptor.ts
@@ -1,13 +1,15 @@
 import { Injectable } from '@angular/core';
 
 import { AuthService } from '../services/auth.service';
-import { HttpHandler, HttpRequest, HttpEvent, HttpInterceptor } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import {HttpHandler, HttpRequest, HttpEvent, HttpInterceptor, HttpErrorResponse} from '@angular/common/http';
+import {Observable, of} from 'rxjs';
+import {catchError} from "rxjs/operators";
+import {Router} from "@angular/router";
 
 @Injectable()
 export class JwtTokenInterceptor implements HttpInterceptor {
 
-  constructor(public auth: AuthService) {}
+  constructor(public auth: AuthService, private router: Router) {}
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     let interceptedRequest = request.clone({
@@ -16,6 +18,14 @@ export class JwtTokenInterceptor implements HttpInterceptor {
       }
     });
 
-    return next.handle(interceptedRequest);
+    return next.handle(interceptedRequest).pipe(catchError(x => this.handleErrors(x)));
+  }
+
+  private handleErrors(err: HttpErrorResponse): Observable<any> {
+    if (err.status === 401) {
+      this.auth.redirectToUrl = this.router.url;
+      this.router.navigate(['/login']);
+      return of(err.message);
+    }
   }
 }
